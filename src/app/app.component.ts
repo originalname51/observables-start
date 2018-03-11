0c9af3af11d9f8f4f14a2256af6e7398824a863a
Robert Stine <robwstine@outlook.com>
2018-03-11 14:00:35 -0400

    Observables course work for reference.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UsersService} from './users.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+
+  customObsSubscription: Subscription;
+  user1Activated  = false;
+  user2Activated = false;
+
+  constructor(private userService: UsersService) {}
+
+  ngOnInit() {
+    this.customObsSubscription = this.userService.userActivated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.user1Activated = true;
+        }
+        if (id === 2) {
+          this.user2Activated = true;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy() {
+      this.customObsSubscription.unsubscribe();
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import {Observer} from 'rxjs/Observer';
+import {Subscription} from 'rxjs/Subscription';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  numbersObservableSubscription: Subscription;
+  customObsSubscription: Subscription;
+
 
   constructor() { }
 
   ngOnInit() {
+    const myNumbers = Observable.interval(1000).map(
+      (data: number) => {
+        return data * 2;
+      }
+    );
+    this.numbersObservableSubscription = myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
+
+    const myObservable = Observable.create(
+      ( observer: Observer<string> ) => {
+        setTimeout( () => {
+          observer.next('first package');
+        }, 2000);
+        setTimeout( () => {
+          observer.next('second package');
+        }, 4000);        setTimeout( () => {
+      //    observer.error('Failed to work');
+          observer.complete(); // dead after this
+        }, 5000);
+      });
+
+    this.customObsSubscription = myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (error: string) => {
+        console.log(error);
+      },
+      () => {console.log('completed'); }
+    );
+  }
+
+  ngOnDestroy() {
+    this.customObsSubscription.unsubscribe();
+    this.numbersObservableSubscription.unsubscribe();
   }
 
 }
